a8c5169ffab35f5bb2fa726e8addd3eafbbb30c5
Fer <20220031@ricaldone.edu.sv>
2024-06-08 12:28:28 -0600

    cambios

--- app/src/main/java/fernanda/hernandez/crudfernadaactividad/MainActivity.kt
+++ app/src/main/java/fernanda/hernandez/crudfernadaactividad/MainActivity.kt
@@ -1,10 +1,19 @@
 package fernanda.hernandez.crudfernadaactividad
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import modelo.claseconexion
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +25,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val txtnombre = findViewById<TextView>(R.id.txtnombre)
+        val txtcontasena = findViewById<TextView>(R.id.txtcontrasena)
+        val btnlogin = findViewById<Button>(R.id.btnlogin)
+        val txtregistrarse = findViewById<TextView>(R.id.txtregistrarse)
+
+        btnlogin.setOnClickListener{
+            CoroutineScope(Dispatchers.Main).launch{
+                val objconexion = claseconexion().cadenaconexion()
+                val addusuario = objconexion?.prepareStatement("insert into usuariosss (UUID, nombre, contraseña) values(?, ?, ?)")!!
+                addusuario.setString(1,UUID.randomUUID().toString())
+                addusuario.setString(2,txtnombre.text.toString())
+                addusuario.setString(3,txtcontasena.text.toString())
+                addusuario.executeUpdate()
+                withContext(Dispatchers.Main){
+                    val helpdesk = Intent(this@MainActivity, helpdesk::class.java)
+                    startActivity(helpdesk)
+                }
+
+            }
+
+        }
+
     }
+
+
 }
--- app/src/main/java/modelo/claseconexion.kt
+++ app/src/main/java/modelo/claseconexion.kt
@@ -7,9 +7,9 @@ class claseconexion {
 
     fun cadenaconexion() : Connection? {
         try {
-            val url = "jdbc:oracle:thin:@10.10.1.168:1521:xe"
-            val usuario = "SYSTEM"
-            val contrasena = "desarrollo"
+            val url = "jdbc:oracle:thin:@192.168.1.14:1521:xe"
+            val usuario = "itzfer_DEVELOPER"
+            val contrasena = "huhyunjinmybeloved"
 
             val connection = DriverManager.getConnection(url, usuario, contrasena)
             return connection
